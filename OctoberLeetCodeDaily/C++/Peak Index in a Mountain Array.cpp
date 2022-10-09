522aaedfb162abb6cf9281fd824445aab274ebb3
ayush-1601 <bansalayush1601@gmail.com>
2022-10-09 15:07:55 +0530

    Created "Peak index in a mountain array" leetcode problem solution.

---
+++ OctoberLeetCodeDaily/C++/Peak Index in a Mountain Array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int max=INT_MIN;
+        int index;
+        for(int i=0; i<arr.size(); i++){
+            if(arr[i]>max){
+                max=arr[i];
+                index=i;
+            }
+        }
+        return index;
+    }
+};
